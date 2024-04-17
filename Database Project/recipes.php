b371ca9609993c89caf82bad80c66f571f76c4c2
abdulmujeeb4ahmed <149419208+abdulmujeeb4ahmed@users.noreply.github.com>
2024-04-17 19:04:39 -0400

    Update recipes.php

--- Database Project/recipes.php
+++ Database Project/recipes.php
@@ -14,7 +14,7 @@
         <meta charset="utf-8">
         <title>RecipesAroundTheWorld</title>
         <meta name="viewport" content="width=device-width, intial-scale = 1.0">
-        <link rel = "stylesheet" href="style.css">
+        <link rel = "stylesheet" href="cuisinepage.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         </label>
     

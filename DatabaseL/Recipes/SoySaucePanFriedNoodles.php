0240de7f67697a7850fdf9d22f1a3d8d862dd7f0
leyyenah <167366349+leyyenah@users.noreply.github.com>
2024-04-23 16:36:08 -0400

    Update SoySaucePanFriedNoodles.php

--- DatabaseL/Recipes/SoySaucePanFriedNoodles.php
+++ DatabaseL/Recipes/SoySaucePanFriedNoodles.php
@@ -3,7 +3,7 @@
 <meta charset="utf-8">
 <title>Soy Sauce Pan Fried Noodles</title>
 <meta name="viewport" content="width=device-width, intial-scale = 1.0">
-<link rel = "stylesheet" href="layout.css">
+<link rel = "stylesheet" href="recipes1.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </label>
 
@@ -13,11 +13,6 @@
 <input type ="checkbox" id="check">
 <label for = "check" class="checkbtn">
     <i class="fa fa-bars"></i>
-</label>
-<a href="index.html" class="logo">
-    <img src="./logo.jpg" alt="RecipesAroundTheWorld" width="165"/>
-</a>
-<label class ="logo"></label>
 <ul>
     <li><a class="active" href ='#'>Home</a></li>
     <li><a href ='cuisine.php'>Cuisines</a></li>
@@ -34,7 +29,6 @@
     // Create connection
     $conn = new mysqli($host, $user, $pass, $dbname);
 
-
     $sql = "SELECT title FROM recipes
         WHERE recipe_id = 1";
         $result = $conn->query($sql);
@@ -45,10 +39,13 @@
             $title = isset($row['title']) ? $row['title'] : '';
             
             // Use $name and $measurement as needed
-            echo "<br><h1><center>$title<center></h1><br><br>";
+            echo "<br><h1><center>$title</center></h1><br><br>";
+            
         }
-
-
+        $imagePath = str_replace(' ', ' ', $title) . ".jpg";
+        echo "<div class='image'>";
+        echo "<img src='./Recipes/$imagePath' alt='$title' style='width: 100%; height: auto;'>";
+        echo "</div>";
     }
     $sql = "SELECT description FROM recipes WHERE recipe_id = 1";
         $result = $conn->query($sql);
@@ -56,14 +53,15 @@
             // Output data
             $row = $result->fetch_assoc();
             $description = isset($row['description']) ? $row['description'] : '';
-            echo "<br><center>$description<center><br>";
+            echo "<div class='description'>";
+            echo "<br><center>$description</center><br>";
         }
-        echo "<h4>Ingredients</h4>";
+        echo "<center><h4>Ingredients</h4><br></center>";
 
       $sql = "SELECT name, measurement FROM ingredient
         WHERE recipe_id = 1";
         $result = $conn->query($sql);
-
+    echo
     if ($result && $result->num_rows > 0) {
         // Output data of each row
         while ($row = $result->fetch_assoc()) {
@@ -74,19 +72,18 @@
             // Use $name and $measurement as needed
             echo "$name - $measurement<br>";
         }
-
     }
-    echo "<br><h4>Instructions<br></h4>";
+    echo "<center><br><h4>Instructions<br></h4></center>";
     echo "<br><br>";
 
-    $sql = "SELECT instructions FROM recipes
-      WHERE recipe_id = 1";
-      $result = $conn->query($sql);
+    $sql = "SELECT instructions FROM recipes WHERE recipe_id = 1";
+    $result = $conn->query($sql);
 
-      while ($valid = $result->fetch_assoc()) {
-        // Output the instructions HTML directly
-        echo '<div class="instructions" style="list-style-type: decimal;">' . $valid['instructions'] . '</div>';
-    }
+    // Change this part
+    while ($valid = $result->fetch_assoc()) {
+        // Output the instructions as list items
+        echo "<div class='instructions'>{$valid['instructions']}</div>";    }
+
     
       $conn->close();
   ?>

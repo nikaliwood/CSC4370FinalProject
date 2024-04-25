73ea2812eda79c83cbcdbefe5982e88d5a520ddd
abdulmujeeb4ahmed <149419208+abdulmujeeb4ahmed@users.noreply.github.com>
2024-04-24 20:41:27 -0400

    Update Mexican.php

--- Database Project/Cuisines/Mexican.php
+++ Database Project/Cuisines/Mexican.php
@@ -13,7 +13,7 @@ $user_name = isset($_SESSION['name']) ? $_SESSION['name'] : '';
 
 <head>
     <meta charset="utf-8">
-    <title>Mexican Cuisine</title>
+    <title>Arab Cuisine</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="origin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">

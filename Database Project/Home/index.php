de7638b4206a3e778c954fc0501e79daf0390717
nikaliwood <167284895+nikaliwood@users.noreply.github.com>
2024-04-25 18:41:41 -0400

    Add files via upload

--- Database Project/Home/index.php
+++ Database Project/Home/index.php
@@ -32,7 +32,7 @@ $user_name = isset($_SESSION['name']) ? $_SESSION['name'] : '';
                 <li><a class="active" href ='index.php'>Home</a></li>
                 <li><a href ='cuisine.php'>Cuisines</a></li>
                 <li><a href ='recipes.php'>Recipes</a></li>
-                <li><a href ='review.php'>Review</a></li>
+                
                 <?php if ($is_logged_in): ?>
                 <!-- Display the personalized greeting -->
                 <li><a href ="account.php">ACCOUNT</a></li>
